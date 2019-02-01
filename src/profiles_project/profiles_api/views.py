32dd6b785d2e7bf84ca870bdbf35a71b6d22d289
Noman Rafique <nomrafique@gmail.com>
2019-02-01 17:46:34 +0500

    Create, Delete, Upadate, Partial Upadate and Destroy methods in ViewSet

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -64,6 +64,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API viewSet."""
 
+    serializer_class = serializers.HelloSerializers
+
     def list(self, request):
         """Return a hello message"""
 
@@ -74,3 +76,36 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message' : 'Hello!', 'a_viewset' : a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+
+        serializer = serializers.HelloSerializers(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message' : message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting by an ID"""
+
+        return Response({'http_method','GET'})
+
+    def update(self, request, pk=None):
+        """Updates an object"""
+
+        return Response({'http_method','PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Partially updates an object"""
+
+        return Response({'http_method','PATCH'})
+
+
+    def destroy(self, request, pk=None):
+        """Deletes an object from the database"""
+
+        return Response({'http_method','DELETE'})
